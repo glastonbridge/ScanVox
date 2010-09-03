3e4fb65d6c4c365ad82324735a0c15006a075790
Dan Stowell <danstowell@users.sourceforge.net>
2010-09-03 16:15:36 +0100

    docstrings

--- src/org/isophonics/scanvox/MappedSynth.java
+++ src/org/isophonics/scanvox/MappedSynth.java
@@ -7,17 +7,17 @@ package org.isophonics.scanvox;
 * pass details through from the synthdef-generation to the Java.
 */
 public abstract class MappedSynth {
-	/*
+	/**
 	 * How many controls the synth has. The actual synthdef always has TWO beforehand (outbus, amp)
 	 * and then this many controls that affect the sound.
 	 */
 	public abstract int getNumControls();
-	/*
+	/**
 	 * Which parameter may we hardwire as pitch? Will return -1 if none, otherwise an index into the number from @getNumControls().
 	 * Remember to add two because of the (outbus, amp) controls.
 	 */
 	public abstract int getParamShouldBePitch();
-	/*
+	/**
 	 * User-visible name for the synth. Short, fun, mysterious, intriguing, pithy, full of character.
 	 */
 	public abstract String getLabel();
